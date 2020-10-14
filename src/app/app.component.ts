cd669191126c9fff11a35a116a8a01f12ad0a3b6
dagomx <dagoxl@gmail.com>
2020-10-14 17:26:48 -0500

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, VERSION } from "@angular/core";
 
 @Component({
   selector: "my-app",
@@ -6,6 +6,7 @@ import { Component } from "@angular/core";
   styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
+
   loader = {
     primaryColor: "#cecece",
     secondaryColor: "#777777"
